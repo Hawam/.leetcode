363d1f9b8e6e0b89e96b91754f94f5afa0ce9c4d
Ahmed Hawam <Ahmed_hwm@hotmail.com>
2023-02-05 12:34:08 +0200

    Update 1.two-sum.cs

--- 1.two-sum.cs
+++ 1.two-sum.cs
@@ -7,7 +7,17 @@
 // @lc code=start
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
-        
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = i+1; j < nums.Length; j++)
+            {
+                if (nums[i]+nums[j]==target)
+                {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
     }
 }
 // @lc code=end
